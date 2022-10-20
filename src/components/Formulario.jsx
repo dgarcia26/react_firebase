80ffcf319987a229f1b6d6d953ec9ebb07ee2ac0
Darwin G <dgarcia26@cuc.edu.co>
2022-10-20 12:10:34 -0500

    Preparando el formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Formulario = () => {
   return (
-    <div>Formulario</div>
+    <div className='container mt-5'>
+        <h1 className='text-center'>CRUD WEB 2</h1>
+        <hr/>
+        <div className="row">
+            <div className="col-8">
+                <h4 className="text-center">Listado de Frutas</h4>
+                <ul className="list-group">
+                
+            </ul>
+            </div>
+            
+        
+
+                
+        </div>
+    </div>
   )
 }
 
